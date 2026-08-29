0cfa7b23e35b17561cd32e39a2e502095bc01a51
Neha <nehaprakash398@gmail.com>
2026-08-29 21:02:05 +0100

    Models is added

---
+++ ControllerWebApi/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MyFirstWebAPIProject.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        // Navigation property (One-to-Many relationship)
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ ControllerWebApi/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using ControllerAndModelWebApi.Models;
+using System.ComponentModel.DataAnnotations;
+namespace MyFirstWebAPIProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [Range(0.01, 10000.00)]
+        public decimal Price { get; set; }
+
+        // Foreign Key for Category
+        public int CategoryId { get; set; }
+
+        // Navigation property (Relationship)
+        public Category Category { get; set; } = null!;
+    }
+}
