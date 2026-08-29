7a7dfda9f46d2c44a207f47ea6ff94b3d6bedb33
Neha <nehaprakash398@gmail.com>
2026-08-29 21:35:52 +0100

    DTOS Added

---
+++ ControllerWebApi/DTOs/ProductDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFirstWebAPIProject.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string CategoryName { get; set; } = null!;
+    }
+}
